d843d040b77d5214d6767dc882fccf99a4aa34ea
Your Name <youremail@example.com>
2025-04-19 22:25:05 -0400

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -15,4 +15,13 @@ const darkModeBtn = document.getElementById('darkModeToggle');
 darkModeBtn.addEventListener('click', function(){
   document.body.classList.toggle('dark-mode');
 });
+const navLinks=document.querySelectorAll('nav a');
+
+for(let i=0;i<navLinks.length;i++){
+  navLinks[i].addEventListener('click',function(e){
+    e.preventDefault();
+    const target=document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({behavior:'smooth'});
+  });
+}
 
